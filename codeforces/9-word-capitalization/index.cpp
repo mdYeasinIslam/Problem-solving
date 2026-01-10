3bd8412632cfe8f3b0727f9dddd4487b6fa0ce71
mdYeasinIslam <hyeasinislam@gmail.com>
2026-01-10 15:25:54 +0600

    solve the word capitalization problem in codeforce

--- codeforces/9-word-capitalization/index.cpp
+++ codeforces/9-word-capitalization/index.cpp
@@ -1,7 +1,20 @@
+/* 
+Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+problem link: https://codeforces.com/problemset/problem/281/A
+*/ 
+
 #include<bits/stdc++.h>
+#include<cstdlib>
 using namespace std;
 
 int main(){
     string str;
-    
+    cin >> str;
+    if(str.length() < pow(10,3)){
+          str[0] = toupper(str[0]);
+          cout << str << endl;
+    }
 }
