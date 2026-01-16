361b51cd910b2f2332ffd748bd16aa38eb9e7050
mdYeasinIslam <hyeasinislam@gmail.com>
2026-01-17 01:28:56 +0600

    init chat room problem -- codeforce-P58

---
+++ codeforces/11-chat_room/index.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+string checkString(string s){
+    int len = s.length();
+    string str;
+
+    cout << str << endl;
+    return str;
+}
+int main(){
+    string s;
+    cin>>s;
+    checkString(s);
+    return 0;
+}
